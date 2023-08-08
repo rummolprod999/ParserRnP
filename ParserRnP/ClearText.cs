9f3bbceb95dd286b5e234329416c1d075e41989b
rummolprod999 <rummolprod999@gmail.com>
2023-08-08 10:29:39 +0300

    new

--- ParserRnP/ClearText.cs
+++ ParserRnP/ClearText.cs
@@ -6,6 +6,15 @@
         {
             string st = s;
             st = st.Replace("ns2:", "");
+            st = st.Replace("ns3:", "");
+            st = st.Replace("ns4:", "");
+            st = st.Replace("ns5:", "");
+            st = st.Replace("ns6:", "");
+            st = st.Replace("ns7:", "");
+            st = st.Replace("ns8:", "");
+            st = st.Replace("ns9:", "");
+            st = st.Replace("ns1:", "");
+            st = st.Replace("ns0:", "");
             st = st.Replace("oos:", "");
             st = st.Replace("", "");
             return st;
