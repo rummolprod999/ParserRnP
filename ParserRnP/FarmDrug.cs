2bdbfba17256ad084a5e05f3f0470c818787ed02
rummolprod999 <rummolprod999@gmail.com>
2023-09-17 18:13:40 +0300

    new

--- ParserRnP/FarmDrug.cs
+++ ParserRnP/FarmDrug.cs
@@ -58,7 +58,7 @@ namespace ParserRnP
                 }
                 finally
                 {
-                    GC.Collect();
+                    //GC.Collect();
                     //GC.WaitForPendingFinalizers();
                 }
                 
